00fb02519ebce094965112d8c6427a4ce59ba945
Nathan Roark <nathanroark@protonmail.com>
2026-09-20 15:34:12 -0700

    fix typo

--- src/config/resume.ts
+++ src/config/resume.ts
@@ -5,7 +5,7 @@ export const Resume = {
   locationLink: "https://www.google.com/maps/place/Seattle,+WA",
   about: "Full Stack Software Engineer",
   summary:
-    "Software engineer creating things I want to exist and working on things I found intesting.",
+    "Software engineer living in Seattle. Creating things I want to exist. Working on things I find intesting.",
   avatarUrl: "/it-me.jpeg",
   personalWebsiteUrl: "https://nathanroark.dev",
   contact: {
